b2050d533de435ccb2f0d4f0af0ca31eb4eacc65
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2022-04-04 22:57:14 -0300

    ajlskhd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './App.css';
 
-import logo from './assets/logo.svg'
+import logo from './assets/logo.png'
 
 
 function App() {
   return (
-   <div className="container">
-     <img src={logo} alt="Aircnc"/>
-     <p>
-       Ofereças <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
-     </p>
-   </div>
+    <div className="container">
+      <img src={logo} alt="Aircnc" />
+      <div className="content">
+        <p>
+          Ofereças <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+
+      </div>
+    </div>
   );
 }
 
